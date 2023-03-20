9109bdb0ce5d43dd0990ecadff28d03f46f9f9ba
QuantOfSolace <baraguzinki@mail.ru>
2023-03-20 18:40:35 +0500

    Добавьте файлы проекта.

---
+++ Project_Balakin/Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Balakin
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+            if (box_login.Text.Length > 0)
+            {
+                if (box_Password.Password.Length > 0)
+                {
+                    /*FIXME*/
+                    ClassChangePage.frame1.Navigate(new Main());
+                    
+                }
+                else MessageBox.Show("Введите пароль");
+            }
+            else MessageBox.Show("Введите логин");
+        }
+
+        private void click_Reg(object sender, RoutedEventArgs e)
+        {
+            ClassChangePage.frame1.Navigate(new Registration());
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if(box_Password.Password.Length > 0) 
+            {
+                Watermark.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Watermark.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ Project_Balakin/Registration.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Balakin
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        private void Check_click(object sender, RoutedEventArgs e)
+        {
+            if (box_login.Text.Length > 0) // проверяем логин
+            {
+                if (box_password.Password.Length > 0) // проверяем пароль
+                {
+                    if (box_password.Password.Length >= 6)
+                    {
+                        bool en = true; // английская раскладка
+                        bool number = false; // цифра
+
+                        for (int i = 0; i < box_password.Password.Length; i++) // перебираем символы
+                        {
+                            if (box_password.Password[i] >= 'А' && box_password.Password[i] <= 'Я') en = false; // если русская раскладка
+                            if (box_password.Password[i] >= '0' && box_password.Password[i] <= '9') number = true; // если цифры
+                        }
+
+                        if (!en)
+                            MessageBox.Show("Доступна только английская раскладка"); // выводим сообщение
+                        else if (!number)
+                            MessageBox.Show("Добавьте хотя бы одну цифру"); // выводим сообщение
+                        if (en && number) // проверяем соответствие
+                        {
+                            if (box_check_password.Password.Length > 0) // проверяем второй пароль
+                            {
+                                if (box_password.Password == box_check_password.Password) // проверка на совпадение паролей
+                                {
+                                    MessageBox.Show("Пользователь зарегистрирован");
+
+                                    
+                                    ClassChangePage.frame1.Navigate(new Login());
+                                }
+                                else MessageBox.Show("Пароли не совпадают");
+                            }
+                            else MessageBox.Show("Повторите пароль");
+                        }
+                    }
+                    else MessageBox.Show("Пароль слишком короткий, минимум 6 символов");
+                }
+                else MessageBox.Show("Укажите пароль");
+            }
+            else MessageBox.Show("Укажите логин");
+        }
+
+        private void Back_click(object sender, RoutedEventArgs e)
+        {
+            ClassChangePage.frame1.Navigate(new Login());
+        }
+
+        private void OnPasswordChanged1(object sender, RoutedEventArgs e)
+        {
+            if (box_password.Password.Length > 0)
+            {
+                Watermark2.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Watermark2.Visibility = Visibility.Visible;
+            }
+        }
+        private void OnPasswordChanged2(object sender, RoutedEventArgs e)
+        {
+            if (box_check_password.Password.Length > 0)
+            {
+                Watermark1.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Watermark1.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
